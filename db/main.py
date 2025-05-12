31592f7d0ee0088183ec0fe2527e9f1004b7d9a6
Matheus Rodrigues <mat.rodf@gmail.com>
2025-05-12 02:40:49 -0300

    Base das tabelas da database

--- db/main.py
+++ db/main.py
@@ -1,6 +1,6 @@
 from typing import Any, Generator
 from sqlmodel import create_engine, SQLModel, Session, select
-from models.yugioh_card import YugiohCard
+from models.yugioh_card import YugiohCard, CardType, MonsterType
 
 DB_FILENAME = "yugioh.db"
 DB_URL = f"sqlite:///{DB_FILENAME}"
@@ -24,11 +24,18 @@ def create_sample_data() -> None:
     # execute only if there is no data in the table
     if not session.exec(select(YugiohCard)).first(): 
       cards = [
-        YugiohCard(id=1, name="Blue-Eyes White Dragon"),
-        YugiohCard(id=2, name="Dark Magician"),
-        YugiohCard(id=3, name="Red-Eyes Black Dragon"),
-        YugiohCard(id=4, name="Exodia the Forbidden One"),
-        YugiohCard(id=5, name="Summoned Skull"),
+        #YugiohCard(id=1, name="Blue-Eyes White Dragon"),
+        #YugiohCard(id=2, name="Dark Magician"),
+        #YugiohCard(id=3, name="Red-Eyes Black Dragon"),
+        #YugiohCard(id=4, name="Exodia the Forbidden One"),
+        #YugiohCard(id=5, name="Summoned Skull"),
+        YugiohCard(id=1, name="Blue-Eyes White Dragon", card_type=CardType.MONSTER, monster_type=MonsterType.DRAGON),
+        YugiohCard(id=2, name="Dark Magician", card_type=CardType.MONSTER, monster_type=MonsterType.SPELLCASTER),
+        YugiohCard(id=3, name="Red-Eyes Black Dragon", card_type=CardType.MONSTER, monster_type=MonsterType.DRAGON),
+        YugiohCard(id=4, name="Exodia the Forbidden One", card_type=CardType.MONSTER, monster_type=MonsterType.SPELLCASTER),
+        YugiohCard(id=5, name="Summoned Skull", card_type=CardType.MONSTER, monster_type=MonsterType.FIEND),
+        YugiohCard(id=6, name="Pot of Greed", card_type=CardType.SPELL),
+        YugiohCard(id=7, name="Mirror Force", card_type=CardType.TRAP)
       ]
       session.add_all(cards)
       session.commit()
