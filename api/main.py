c60300709e03e21b1755f28698f4868b2781ed09
Richard <richard.mzrosa@gmail.com>
2025-05-14 16:45:26 -0300

    delete db only for debug mode

--- api/main.py
+++ api/main.py
@@ -7,7 +7,7 @@ from db.main import init_db, delete_db, create_sample_data
 async def lifespan(app: FastAPI):
   """Initialize the db and create sample data (if there is no data) on api startup."""
   init_db()
-  delete_db()
+  #delete_db()
   create_sample_data()
   yield
   # optionally, add cleanup (free resource) code here if needed
